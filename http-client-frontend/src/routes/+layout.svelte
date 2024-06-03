bef0349a1ddcb0b01293c796ad7c4de78e34d849
daniel.brown <daniel.brown@autotrader.co.uk>
2024-06-03 18:11:54 +0100

    added tauri

--- http-client-frontend/src/routes/+layout.svelte
+++ http-client-frontend/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.postcss';
-
+    export const prerender = true
+    export const ssr = false
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
