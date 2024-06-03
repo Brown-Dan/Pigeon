b42636000f7ad7a1efaf4341e2d31eb6b6c3e015
daniel.brown <daniel.brown@autotrader.co.uk>
2024-06-03 21:10:49 +0100

    added response object in rust backend, added codeblock to main page, added tabs to main page

--- http-client-frontend/src-tauri/src/main.rs
+++ http-client-frontend/src-tauri/src/main.rs
@@ -1,20 +1,40 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use reqwest;
+use serde::{Deserialize, Serialize};
 
 fn main() {
-  tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![send_request])
-      .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![send_request])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    status: u16,
+    size: String,
+    body: String
 }
 
 #[tauri::command]
 async fn send_request(url: String) -> String {
-  let response = reqwest::get(url).await;
-  let response = match response {
-    Ok(response) => response,
-    Err(e) => return e.to_string(),
-  };
-  return response.text().await.expect("There was an error.");
+    let response = reqwest::get(url).await;
+    let response = match response {
+        Ok(response) => response,
+        Err(e) => return e.to_string(),
+    };
+    let status = response.status().as_u16();
+    let body = match response.text().await {
+        Ok(body) => body,
+        Err(e) => return e.to_string(),
+    };
+    let size = body.len().to_string();
+    let my_response = Response {
+        status,
+        body,
+        size,
+    };
+    return serde_json::to_string(&my_response).expect("Error");
 }
--- http-client-frontend/src/routes/+page.svelte
+++ http-client-frontend/src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/tauri';
+	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+	import { CodeBlock } from '@skeletonlabs/skeleton';
 
-	let content: string = "Send a Request"
+	let response: JSON;
+	let tabSet: number = 0;
 
 	function send_request() {
 		const url: string =  (<HTMLInputElement>document.getElementById("url")).value;
 		console.log(url)
 		 invoke("send_request", { url }).then(value => {
 			 if (typeof value === 'string') {
-				 content = JSON.stringify(JSON.parse(value), undefined, 2);
+				 response = JSON.parse(value);
 			 }
 			 console.log(value);
 		 }
@@ -26,8 +29,35 @@
 		<input type="text" id="url" class="text-black">
 		<button on:click={send_request}>Send Request</button>
 	</div>
-	<div class="border-white bg-slate-200  min-w-screen min-h-screen text-black m-5 rounded-2xl p-4 text-wrap">
-		<pre class="whitespace-pre-line">{content}</pre>
+	<div class="m-5">
+		<div>
+			{#if response}
+				<p>Response Status: {response.status}</p>
+			{:else}
+					<p>Response Status: </p>
+			{/if}
+		</div>
+		<TabGroup>
+			<Tab bind:group={tabSet} name="tab1" value={0}>
+				<svelte:fragment slot="lead">Response Body</svelte:fragment>
+			</Tab>
+			<Tab bind:group={tabSet} name="tab2" value={1}>Headers</Tab>
+			<Tab bind:group={tabSet} name="tab3" value={2}>Cookies</Tab>
+			<!-- Tab Panels --->
+			<svelte:fragment slot="panel">
+				{#if tabSet === 0}
+					<div class="border-white bg-slate-200 min-w-screen min-h-screen text-black rounded-2xl text-wrap p-5">
+						{#if response}
+							<CodeBlock language="json" code={response.body}></CodeBlock>
+						{/if}
+					</div>
+				{:else if tabSet === 1}
+					<CodeBlock language="html" code={`<div>This is meta</div>`}></CodeBlock>
+				{:else if tabSet === 2}
+					(tab panel 3 contents)
+				{/if}
+			</svelte:fragment>
+		</TabGroup>
 	</div>
 </div>
 
