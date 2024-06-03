2491540f0eb9986864edd330ec0aff973e5187b2
daniel.brown <daniel.brown@autotrader.co.uk>
2024-06-03 18:03:58 +0100

    svelte init

---
+++ http-client-frontend/tailwind.config.ts
@@ -0,0 +1,41 @@
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms';
+import { skeleton } from '@skeletonlabs/tw-plugin'
+
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'crimson',
+						enhancements: true,
+					},
+					{
+						name: 'hamlindigo',
+						enhancements: true,
+					},
+					{
+						name: 'modern',
+						enhancements: true,
+					},
+					{
+						name: 'wintry',
+						enhancements: true,
+					},
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
+} satisfies Config;
