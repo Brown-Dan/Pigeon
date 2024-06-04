079e0e6408ece9da220159dc3c087838066ade0a
daniel.brown <daniel.brown@autotrader.co.uk>
2024-06-04 01:09:14 +0100

    added spinner and functionality for query params

--- http-client-frontend/src/routes/+page.svelte
+++ http-client-frontend/src/routes/+page.svelte
@@ -18,12 +18,25 @@
 	const exampleData: string = 'https://httpbin.org/get';
 
 	function send_request() {
+		const btn_spinner = document.getElementById("btn_spinner")
+    btn_spinner.removeAttribute("hidden")
+		const btn_content = document.getElementById("btn_content")
+		btn_content?.setAttribute("hidden", "hidden")
+		let queryParams = "?"
+		if (numOfParams > 1) {
+		for (let i = 0; i < numOfParams; i++) {
+			let param_name = <HTMLInputElement>document.getElementById('param_name_' + i).value;
+			let param_value = <HTMLInputElement>document.getElementById('param_value_' + i).value;
+			queryParams = queryParams + param_name  + "=" + param_value + "&"
+		}}
 		const url: string = (<HTMLInputElement>document.getElementById('url')).value;
 		let start_time = window.performance.now();
-		invoke('send_request', { url }).then(value => {
+		invoke('send_request', { url: url + queryParams }).then(value => {
 				let end_time = window.performance.now();
 				time = end_time - start_time;
-				if (typeof value === 'string') {
+			btn_content?.removeAttribute("hidden")
+			btn_spinner?.setAttribute("hidden", "hidden")
+			if (typeof value === 'string') {
 					response = JSON.parse(value);
 				}
 				headers = new Map(Object.entries(response.headers));
@@ -99,7 +112,10 @@
 				{/if}
 			</svelte:fragment>
 		</TabGroup>
-		<button on:click={send_request} type="button" class="btn btn-xl variant-filled mt-5 text">Send</button>
+		<button id="send_request_btn" on:click={send_request} type="button" class="btn btn-xl variant-filled mt-5 text">
+			<svg id="btn_spinner" hidden aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
+			<b id="btn_content" hidden="">Send</b>
+		</button>
 	</div>
 	<div class="m-5">
 		<div>
