2d3f99a946282c310a2b2ab0ea8e55f16c149fb3
daniel.brown <daniel.brown@autotrader.co.uk>
2024-06-10 22:01:51 +0100

    styling changes

--- http-client-frontend/src/lib/MethodUtils.ts
+++ http-client-frontend/src/lib/MethodUtils.ts
@@ -5,3 +5,18 @@ export const method_to_colour: Map<string, string> = new Map([
 	["DELETE", "variant-filled-primary"],
 	["PATCH", "variant-filled-warning"]
 ])
+
+export const method_to_abb: Map<string, string> = new Map([
+	["GET", "GET"],
+	["POST", "PST"],
+	["PUT", "PUT"],
+	["DELETE", "DEL"],
+	["PATCH", "PTC"]
+])
+
+export function limit_request_chars(name: string): string {
+	if (name.length > 15) {
+		return name.substring(0, 12) + "...";
+	}
+	return name;
+}
--- http-client-frontend/src/lib/components/Sidebar.svelte
+++ http-client-frontend/src/lib/components/Sidebar.svelte
@@ -17,7 +17,7 @@
 	import AddCollectionModal from '$lib/components/modals/AddCollectionModal.svelte';
 	import { requests } from '$lib/RequestsStore';
 	import AddRequestModal from '$lib/components/modals/AddRequestModal.svelte';
-	import { method_to_colour } from '$lib/MethodUtils';
+	import { limit_request_chars, method_to_abb, method_to_colour } from '$lib/MethodUtils';
 
 
 	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
@@ -93,120 +93,105 @@
 </div>
 
 <div class="overflow-hidden">
-	<select class="select mr-5 mt-5 ml-3 p-2 text-xs variant-filled hidden lg:inline-block" id="method">
+	<select class="select mr-5 mt-5 ml-2 p-2 text-xs hidden lg:inline-block w-24" id="method">
 		<option value="GET">LOCAL</option>
 		<option value="PUT">PREPROD</option>
 		<option value="PATCH">PROD</option>
 	</select>
-	<div class="ml-3 btn-group variant-filled mt-5 hidden lg:inline-block">
-		<button on:click={() => 	modalStore.trigger(addCollectionModal)}>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-					 class="size-6">
-				<path stroke-linecap="round" stroke-linejoin="round"
-							d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+
+	<div class="ml-2 btn-group variant-filled mt-4 hidden lg:inline-block">
+		<button on:click={() => modalStore.trigger(addCollectionModal)} class="p-1">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
 			</svg>
 		</button>
-		<button on:click={add_request}>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="#000000" class="size-6" viewBox="0 0 482.14 482.14"
-					 xml:space="preserve">
-					<g>
-						<path
-							d="M302.599,0H108.966C80.66,0,57.652,23.025,57.652,51.315v379.509c0,28.289,23.008,51.315,51.314,51.315h264.205   c28.275,0,51.316-23.026,51.316-51.315V121.449L302.599,0z M373.171,450.698H108.966c-10.969,0-19.89-8.905-19.89-19.874V51.315   c0-10.953,8.921-19.858,19.89-19.858l181.875-0.189v67.218c0,19.653,15.949,35.603,35.588,35.603l65.877-0.189l0.725,296.925   C393.03,441.793,384.142,450.698,373.171,450.698z" />
-						<path
-							d="M241.054,150.96c-49.756,0-90.102,40.347-90.102,90.109c0,49.764,40.346,90.11,90.102,90.11   c49.771,0,90.117-40.347,90.117-90.11C331.171,191.307,290.825,150.96,241.054,150.96z M273.915,253.087h-20.838v20.835   c0,6.636-5.373,12.017-12.023,12.017c-6.619,0-12.01-5.382-12.01-12.017v-20.835H208.21c-6.637,0-12.012-5.383-12.012-12.018   c0-6.634,5.375-12.017,12.012-12.017h20.834v-20.835c0-6.636,5.391-12.018,12.01-12.018c6.65,0,12.023,5.382,12.023,12.018v20.835   h20.838c6.635,0,12.008,5.383,12.008,12.017C285.923,247.704,280.55,253.087,273.915,253.087z" />
-					</g>
-					</svg>
+		<button on:click={add_request} class="p-1">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="#000000" class="w-5 h-5" viewBox="0 0 482.14 482.14" xml:space="preserve">
+            <g>
+                <path d="M302.599,0H108.966C80.66,0,57.652,23.025,57.652,51.315v379.509c0,28.289,23.008,51.315,51.314,51.315h264.205c28.275,0,51.316-23.026,51.316-51.315V121.449L302.599,0z M373.171,450.698H108.966c-10.969,0-19.89-8.905-19.89-19.874V51.315c0-10.953,8.921-19.858,19.89-19.858l181.875-0.189v67.218c0,19.653,15.949,35.603,35.588,35.603l65.877-0.189l0.725,296.925C393.03,441.793,384.142,450.698,373.171,450.698z" />
+							<path d="M241.054,150.96c-49.756,0-90.102,40.347-90.102,90.109c0,49.764,40.346,90.11,90.102,90.11c49.771,0,90.117-40.347,90.117-90.11C331.171,191.307,290.825,150.96,241.054,150.96z M273.915,253.087h-20.838v20.835c0,6.636-5.373,12.017-12.023,12.017c-6.619,0-12.01-5.382-12.01-12.017v-20.835H208.21c-6.637,0-12.012-5.383-12.012-12.018c0-6.634,5.375-12.017,12.012-12.017h20.834v-20.835c0-6.636,5.391-12.018,12.01-12.018c6.65,0,12.023,5.382,12.023,12.018v20.835h20.838c6.635,0,12.008,5.383,12.008,12.017C285.923,247.704,280.55,253.087,273.915,253.087z" />
+            </g>
+        </svg>
 		</button>
-		<button>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-					 class="size-6">
-				<path stroke-linecap="round" stroke-linejoin="round"
-							d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+		<button class="p-1">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
 			</svg>
 		</button>
-		<button>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-					 class="size-6">
-				<path stroke-linecap="round" stroke-linejoin="round"
-							d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+		<button class="p-1">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+				<path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
 			</svg>
 		</button>
 	</div>
+
 </div>
-<div class="overflow-y-auto overscroll-none mb-5 overflow-x-hidden">
+<div class="overflow-y-auto overscroll-none mb-5 overflow-x-hidden ml-0">
 	{#if requests_result}
 		<TreeView class="hidden lg:block text-xs">
-			{#each requests_result.collections as collection}
-				<TreeViewItem class="my-0.5">
-					<div class="flex items-center justify-between">
-						<span class="flex items-center whitespace-nowrap overflow-hidden text-sm">{collection.name}</span>
-						<button
-							class="ml-1 p-1 flex items-center justify-center"
-							on:click={(event) => {
-                        event.stopPropagation();
-                        selected_collection = collection.name;
-                    }}
-							use:popup={collectionSettingsPopup}
-						>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-									 stroke="currentColor" class="w-4 h-4">
-								<path stroke-linecap="round" stroke-linejoin="round"
-											d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-							</svg>
-						</button>
+			<div class="flex items-center justify-between">
+				{#each requests_result.collections as collection}
+					<div class="max-w-28">
+						<TreeViewItem class="my-0.5">
+							<div class="items-center">
+								<span class="whitespace-nowrap overflow-hidden text-xs">{collection.name}</span>
+							</div>
+							<svelte:fragment slot="children">
+								<div>
+									{#each collection.requests as request}
+										<div class="flex items-center justify-between">
+											<div class="flex-grow">
+												<TreeViewItem class="my-0.5" on:click={() => open_request_tab(request)}>
+													<div class="flex items-center">
+														<button
+															class="p-1 flex items-center justify-center"
+															on:click={() => {selected_collection = request.name}}
+															use:popup={collectionSettingsPopup}
+														>
+															<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+																	 stroke="currentColor" class="w-4 h-4">
+																<path stroke-linecap="round" stroke-linejoin="round"
+																			d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+															</svg>
+														</button>
+														<span class="ml-1 badge {method_to_colour.get(request.method)} mr-2 text-xs">{method_to_abb.get(request.method)}</span>
+														<span class="overflow-hidden whitespace-nowrap text-xs">{limit_request_chars(request.name)}</span>
+													</div>
+												</TreeViewItem>
+											</div>
+										</div>
+									{/each}
+								</div>
+							</svelte:fragment>
+						</TreeViewItem>
 					</div>
-					<svelte:fragment slot="children">
-						{#each collection.requests as request}
+				{/each}
+			</div>
+			<div>
+				{#each requests_result.orphaned_requests as request}
+					<div class="flex items-center justify-between">
+						<div class="flex-grow">
 							<TreeViewItem class="my-0.5" on:click={() => open_request_tab(request)}>
-								<div class="flex items-center justify-between">
-									<div class="flex items-center">
-										<span class="ml-1 badge {method_to_colour.get(request.method)} mr-2 text-xs">{request.method}</span>
-										<span class="overflow-hidden whitespace-nowrap text-sm">{request.name}</span>
-									</div>
+								<div class="flex items-center">
 									<button
 										class="p-1 flex items-center justify-center"
-										on:click={(event) => {
-                                    event.stopPropagation();
-                                    selected_collection = request.collection_name;
-                                }}
+										on:click={() => {selected_collection = request.name}}
 										use:popup={collectionSettingsPopup}
 									>
-										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
 												 stroke="currentColor" class="w-4 h-4">
 											<path stroke-linecap="round" stroke-linejoin="round"
 														d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 										</svg>
 									</button>
+									<span class="ml-1 badge {method_to_colour.get(request.method)} mr-2 text-xs">{method_to_abb.get(request.method)}</span>
+									<span class="overflow-hidden whitespace-nowrap text-xs">{request.name}</span>
 								</div>
 							</TreeViewItem>
-						{/each}
-					</svelte:fragment>
-				</TreeViewItem>
-			{/each}
-			{#each requests_result.orphaned_requests as request}
-				<TreeViewItem class="my-0.5" on:click={() => open_request_tab(request)}>
-					<div class="flex items-center justify-between">
-						<div class="flex items-center">
-							<span class="ml-1 badge {method_to_colour.get(request.method)} mr-2 text-xs">{request.method}</span>
-							<span class="overflow-hidden whitespace-nowrap text-sm">{request.name}</span>
 						</div>
-						<button
-							class="p-1 flex items-center justify-center"
-							on:click={(event) => {
-                        event.stopPropagation();
-                        selected_collection = request.collection_name;
-                    }}
-							use:popup={collectionSettingsPopup}
-						>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-									 stroke="currentColor" class="w-4 h-4">
-								<path stroke-linecap="round" stroke-linejoin="round"
-											d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-							</svg>
-						</button>
 					</div>
-				</TreeViewItem>
-			{/each}
+				{/each}
+			</div>
 		</TreeView>
 	{/if}
 </div>
